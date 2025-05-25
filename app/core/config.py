bf96c7622c3a4d6974f6066a445bc514084f6b88
DragaoTI <140221509+DragaoTI@users.noreply.github.com>
2025-05-25 14:09:43 +0000

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,6 @@
 import os
 import secrets
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 from typing import Optional, List, Dict, Any, Union
 from datetime import timedelta
 import json
