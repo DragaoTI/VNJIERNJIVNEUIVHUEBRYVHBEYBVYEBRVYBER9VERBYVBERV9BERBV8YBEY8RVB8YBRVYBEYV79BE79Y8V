92b9eb59ddfbcacda680fd8c91e825c5b01a4316
DragaoTI <140221509+DragaoTI@users.noreply.github.com>
2025-05-25 22:35:32 +0000

    Update __init__.py

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -3,4 +3,4 @@ from app.routers.admin_router import router as admin_router
 from app.routers.admin_panel_router import admin_panel_router
 from app.routers.two_factor_router import router as two_factor_router  
 from app.routers.security_router import router as security_router
-
+from app.routers.stats_router import router as stats_router
